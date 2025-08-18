5d3d58bd9bcc52c76711bdd2a1a2e71773dcef9d
Timothy Flavin <BinaryOwlizard@gmail.com>
2025-08-18 17:43:18 -0400

    damn2

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+import sys
+from setuptools import setup, Extension, find_packages
+import pybind11
+
+# Define the C++ extension module
+ext_modules = [
+    Extension(
+        "cooppush.cooppush_cpp",
+        ["cpp_src/backend.cpp"],
+        include_dirs=[pybind11.get_include()],
+        language="c++",
+        extra_compile_args=(
+            ["-std=c++11"] if sys.platform != "win32" else ["/std:c++14"]
+        ),
+    ),
+]
+setup(ext_modules=ext_modules, name="cooppush", version="1.0.3")
